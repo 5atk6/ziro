3b7e13487cc37347e5afbae2ffa5948d934774c1
atk <atksuimiy656@gmail.com>
2016-10-01 05:18:21 +0000

    success get distance

--- app.rb
+++ app.rb
@@ -7,9 +7,11 @@ require 'sinatra/activerecord'
 require './models'
 require 'sinatra/cross_origin'
 
-# configure do
-  enable :cross_origin
-# end
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+enable :cross_origin
 
 set :allow_origin, :any
 set :allow_methods, [:get, :post, :options]
@@ -23,12 +25,44 @@ options "*" do
   200
 end
 
-get '/search/:name' do
-    cross_origin
-    name = paramas[:name]
-    
+API_URL_PREFIX = "http://distance.search.olp.yahooapis.jp/OpenLocalPlatform/V1/distance?coordinates="
+API_ID = " &appid=dj0zaiZpPVFjOTVWRk15NHZjNCZzPWNvbnN1bWVyc2VjcmV0Jng9NDY-&output=json"
+
+get '/search/:place' do
+  cross_origin
+  user_place = params[:place]
+  uri = URI("http://distance.search.olp.yahooapis.jp/OpenLocalPlatform/V1/distance")
+
+  Ziro.all.each do |ziro|
+    latitude = ziro.latitude
+    longitude = ziro.longitude
+    place = latitude.to_s + "," + longitude.to_s
+    uri.query = URI.encode_www_form({
+      appid: "dj0zaiZpPVFjOTVWRk15NHZjNCZzPWNvbnN1bWVyc2VjcmV0Jng9NDY-",
+      coordinates: user_place + " " + place,
+      output: "json"
+    })
+    res = Net::HTTP.get_response(uri)
+    returned_json = JSON.parse(res.body)
+  end
+  erb :index
 end
 
-post '/test' do
-  'ttt'
+
+get '/test' do
+  cross_origin
+  user_place = "139.701860,35.657846"
+  uri = URI("http://distance.search.olp.yahooapis.jp/OpenLocalPlatform/V1/distance")
+
+  place = "139.693770,35.655091"
+  uri.query = URI.encode_www_form({
+    appid: "dj0zaiZpPVFjOTVWRk15NHZjNCZzPWNvbnN1bWVyc2VjcmV0Jng9NDY-",
+    coordinates: user_place + " " + place,
+    output: "json"
+  })
+  res = Net::HTTP.get_response(uri)
+  distance = JSON.parse(res.body)["Feature"][0]["Geometry"]["Distance"]
+  
+  # json returned_json
+  erb :index
 end
