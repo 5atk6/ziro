52dc058cee58dab7d1b9ff0c1926bdf667264ebb
atk <atksuimiy656@gmail.com>
2016-10-01 07:15:44 +0000

    searchアクションの高速化

--- app.rb
+++ app.rb
@@ -28,19 +28,29 @@ end
 get '/search/:place' do
   cross_origin
   user_place = params[:place]
-  uri = URI("http://distance.search.olp.yahooapis.jp/OpenLocalPlatform/V1/distance")
+  user_latitude = user_place.split(",")[1]
+  user_longitude = user_place.split(",")[0]
+  # uri = URI("http://distance.search.olp.yahooapis.jp/OpenLocalPlatform/V1/distance")
+  # near_ziro = Array.new(38)
 
   Ziro.all.each do |ziro|
     latitude = ziro.latitude
     longitude = ziro.longitude
-    place = longitude.to_s + "," + latitude.to_s
-    uri.query = URI.encode_www_form({
-      appid: "dj0zaiZpPVFjOTVWRk15NHZjNCZzPWNvbnN1bWVyc2VjcmV0Jng9NDY-",
-      coordinates: user_place + " " + place,
-      output: "json"
-    })
-    res = Net::HTTP.get_response(uri)
-    _distance = JSON.parse(res.body)["Feature"][0]["Geometry"]["Distance"]
+    diff_latitude = user_latitude.to_f - latitude  
+    diff_longtitude =  user_longitude.to_f - longitude
+    added_distance =  diff_latitude**2 + diff_longtitude**2
+    
+    _distance = (Math.sqrt(added_distance)) * 111263.283
+    # near_ziro << {ziro.name, ziro.longitude, ziro.latitude, ziro.imageurl, _distance}
+    # place = longitude.to_s + "," + latitude.to_s
+    # uri.query = URI.encode_www_form({
+    #   appid: "dj0zaiZpPVFjOTVWRk15NHZjNCZzPWNvbnN1bWVyc2VjcmV0Jng9NDY-",
+    #   coordinates: user_place + " " + place,
+    #   output: "json"
+    # })
+    # res = Net::HTTP.get_response(uri)
+    # _distance = JSON.parse(res.body)["Feature"][0]["Geometry"]["Distance"]
+    
     ziro.update({
       distance: _distance
     })
